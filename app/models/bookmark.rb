f09221e7750fd6e9b4da4709e6ec621638c68980
David Cahez <cahez.david@gmail.com>
2024-03-23 13:59:41 +1000

    corrected validations and added movie seeding from API

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-  # validates :movie_id, presence: true
+  validates :movie_id, presence: true
   # validates :list_id, presence: true
   validates_uniqueness_of :movie_id, scope: :list_id
   validates :comment, length: { minimum: 6 }
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,23 +2,4 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
-
-  before_destroy :check_bookmarks
-
-  private
-
-  def check_bookmarks
-    return unless Bookmark.where(movie_id: id).count.zero?
-
-    bookmarks_w_movie = Bookmark.where(movie_id: id).count
-    ending = 's' if bookmarks_w_movie > 1
-    ending ||= ''
-
-    errors.add(
-      :base,
-      :undestroyable,
-      message: "Movie is referenced in #{bookmarks_w_movie} bookmark#{ending}"
-    )
-    throw :abort
-  end
 end

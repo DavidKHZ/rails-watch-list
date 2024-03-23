f7dea261923526b68249ed14cba4ffbbf1840863
David Cahez <cahez.david@gmail.com>
2024-03-23 15:58:33 +1000

    used modal to remove movie from the list

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,29 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new(list_id:params[:list_id])
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list_id = params[:list_id]
+
+    if @bookmark.save
+      redirect_to list_path(@bookmark.list_id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,6 +9,12 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
+
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
